e6c3503d227f268ed098842998aa8e92d4611ded
thomasmccaffery <tom@thomasmccaffery.com>
2014-07-16 14:47:42 -0400

    Database Structure (SQL) Update
    A new table "Play_Times" is added with this revision.  It is used to
    keep track of unique times when a song is played. This blocks occasional
    double repeats from throwing off the statistics.

--- PlayList_MySQL_Structure.sql
+++ PlayList_MySQL_Structure.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 15, 2014 at 05:01 PM
+-- Generation Time: Jul 16, 2014 at 02:39 PM
 -- Server version: 5.5.37-cll
 -- PHP Version: 5.4.23
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `tqrgpjyc_XM_BPM_Playlist`
+-- Database: `XM_BPM_Playlist`
 --
 
 -- --------------------------------------------------------
@@ -30,8 +30,21 @@ CREATE TABLE IF NOT EXISTS `PlayList` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `SongTitle` text NOT NULL,
   `Timed` text NOT NULL,
+  `Count` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=20 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=97 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Play_Times`
+--
+
+CREATE TABLE IF NOT EXISTS `Play_Times` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Timed` text NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=105 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
