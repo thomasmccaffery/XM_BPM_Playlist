47edaf7b21904dfc9d297cc2a8ed1613d0c0b81f
thomasmccaffery <tom@thomasmccaffery.com>
2014-07-21 23:25:34 -0400

    Live Radio Page
    Takes the Current playing song on XM BPM from twitter, searches youtube
    for a video, and starts playing it.  Much like an online radio by
    parsing the data from twitter/youtube.

---
+++ Live.php
@@ -0,0 +1,36 @@
+<?
+/*
+X- AJAX Function to get last song played
+X- Youtube function to get that song on video
+
+- Move last song to sidebar, play current song
+
+- 
+
+Steps:
+
+X- Start
+/- Ajax request recent song [check if a song or tweet*]
+X- Play song on youtube embed
+- Put last song onto sidebar
+- After song ends --> request next song [check if a song or tweet*]
+
+* = If statement to show wait for commercial on radio (seek song before it if different)
+
+
+- make look nice
+- link to other main page list
+*/
+?>
+<!doctype html>
+<html>
+  <head>
+    <title>XM_BPM_Playlist Live</title>
+  </head>
+  <body>
+	<iframe id="Now_Playing" width="560" height="315" src="//www.youtube.com/embed/" frameborder="0" allowfullscreen></iframe>
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+	<script type="text/javascript" src="./inc/auth.js"></script>
+	<script type="text/javascript" src="./assets/xm.js"></script>
+  </body>
+</html>
