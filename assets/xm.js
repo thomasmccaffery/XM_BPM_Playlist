325784b26cf71a6b729255c26e4db19ded7de3b6
thomasmccaffery <tom@thomasmccaffery.com>
2014-07-22 18:15:19 -0400

    Youtube API Addition
    Added pulling of youtube videos via Ajax requests of YouTube_ID.php, to
    get the newest songs youtube video ID.  It then checks if the video
    currently playing as ended. If ended, then the AJAX is polled again to
    see if a new song is played, which is then loaded into the youtube
    player. Thus creating a video radio via twitter feed.

--- assets/xm.js
+++ assets/xm.js
@@ -1,4 +1,38 @@
 
+function onPlayerStateChange(event) {
+    switch(event.data) {
+        case YT.PlayerState.ENDED:
+			YouTubeID(CurrentSong()); /* After Song Ends - Get the new now currently playing song */
+            break;
+        case YT.PlayerState.PLAYING:
+            break;
+        case YT.PlayerState.PAUSED:
+            break;
+        case YT.PlayerState.BUFFERING:
+            break;
+        case YT.PlayerState.CUED:
+            break;
+        default:
+            break;
+    }
+}
+
+jQuery(document).ready(function($) {
+	$('.live').bind('contentchanged', function(e) {
+        var vidId = $('.live').attr('id');
+        $('#container').html('<iframe id="player_'+vidId+
+            '" width="420" height="315" src="http://www.youtube.com/embed/'+
+            vidId+'?enablejsapi=1&autoplay=1&autohide=1&showinfo=0" '+
+            'frameborder="0" allowfullscreen></iframe>');
+
+        new YT.Player('player_'+vidId, {
+            events: {
+                'onStateChange': onPlayerStateChange
+            }
+        });
+    });
+});
+
 /* Ajax-Twitter Function [Gets the currently playing song title] */
 function CurrentSong() {
 	var Songd = $.ajax({
@@ -16,26 +50,23 @@ function CurrentSong() {
 function YouTubeID(SongRequest) {
 	var YTID = $.ajax({
 		type:'GET',
+		dataType: 'json',
 		url: "assets/ajax/YouTube_ID.php",
 		data:"q="+SongRequest,
 		async: false,
 		context: document.body,
 		success: function(YTD){
 			console.log(YTD);
-			$('#Now_Playing').attr('src', '//www.youtube.com/embed/'+YTD+'?autoplay=1');
+			/*$('#Now_Playing').attr('src', '//www.youtube.com/embed/'+YTD['ID']+'?autoplay=1');
+			YTD['Time'];*/
+			$('.live').attr('id', YTD['ID']);
+			$('.live').trigger('contentchanged');
+			/* Check if song is the same title -- then wait 1 minute, try again. */
 		}
 	});
-	return YTID.responseText;
 }
-
 
 /* On Start - Get Current Playing Song */
 $( document ).ready(function() {
-	var NewSong = YouTubeID(CurrentSong());
+	YouTubeID(CurrentSong());
 });
-
-/* After Song Finished - Get the now current playing song */
-/* Check if song is the same title -- then wait 1 minute, try again. */
-
-
-
