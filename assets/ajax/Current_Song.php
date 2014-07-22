7b669940c070ea427cb15ba29dce5c70a312e19b
thomasmccaffery <tom@thomasmccaffery.com>
2014-07-21 23:27:01 -0400

    Twitter Search
    Pulls the most recent tweet, which is the current song playing on radio.

---
+++ assets/ajax/Current_Song.php
@@ -0,0 +1,29 @@
+<?
+
+session_start();
+require_once("../../twitteroauth-master/twitteroauth/twitteroauth.php"); /* Path to twitteroauth library */
+include_once '../../inc/connection.php';   /* DB Connection */
+ 
+$twitteruser = "bpm_playlist";
+$notweets = 1;
+$consumerkey = "key";
+$consumersecret = "secret";
+$accesstoken = "token";
+$accesstokensecret = "secret_token";
+ 
+function getConnectionWithAccessToken($cons_key, $cons_secret, $oauth_token, $oauth_token_secret) {
+  $connection = new TwitterOAuth($cons_key, $cons_secret, $oauth_token, $oauth_token_secret);
+  return $connection;
+}
+ 
+$connection = getConnectionWithAccessToken($consumerkey, $consumersecret, $accesstoken, $accesstokensecret);
+ 
+$json = $connection->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=".$twitteruser."&count=".$notweets);
+$Current_Song = strstr($json[0]->text, "playing",true); /* Gets Song Title before "playing ..." */
+$Current_Song = str_replace("#bpmBreaker","",$Current_Song); /* Remove Hashtags */
+		
+echo $Current_Song;
+
+mysqli_close($mysqli);
+
+?>
