16f2b01ab63ad60f6a8a0e91c0fabd42cedf786f
thomasmccaffery <tom@thomasmccaffery.com>
2014-07-21 23:24:00 -0400

    SQL Connection
    MySQLi Connection

---
+++ inc/connection.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * These are the database login details
+ */  
+define("HOST", "localhost");     // The host you want to connect to.
+define("USER", "User_Here");    // The database username. 
+define("PASSWORD", "Pass_Here");    // The database password. 
+define("DATABASE", "XM_BPM_Playlist");    // The database name. 
+
+$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
+$mysqli->query("SET NAMES 'utf8'");
+
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+?>
