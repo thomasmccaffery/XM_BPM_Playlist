f0d7c2e29857c0cafa73b3b9a7ce3a9d4c75ab6a
thomasmccaffery <tom@thomasmccaffery.com>
2014-07-29 13:45:20 -0400

    Added Commercial Break Checks
    Checks if current song is a non-song tweet (commercial or otherwise) and
    then returns "Commercial" as the answer so that the Youtube player does
    not play random videos.

--- assets/ajax/Current_Song.php
+++ assets/ajax/Current_Song.php
@@ -1,25 +1,29 @@
-<?
-
-session_start();
-require_once("../../twitteroauth-master/twitteroauth/twitteroauth.php"); /* Path to twitteroauth library */
-include_once '../../inc/connection.php';   /* DB Connection */
- 
-$twitteruser = "bpm_playlist";
-$notweets = 1;
- 
-function getConnectionWithAccessToken($cons_key, $cons_secret, $oauth_token, $oauth_token_secret) {
-  $connection = new TwitterOAuth($cons_key, $cons_secret, $oauth_token, $oauth_token_secret);
-  return $connection;
-}
- 
-$connection = getConnectionWithAccessToken($consumerkey, $consumersecret, $accesstoken, $accesstokensecret);
- 
-$json = $connection->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=".$twitteruser."&count=".$notweets);
-$Current_Song = strstr($json[0]->text, "playing",true); /* Gets Song Title before "playing ..." */
-$Current_Song = str_replace("#bpmBreaker","",$Current_Song); /* Remove Hashtags */
-		
-echo $Current_Song;
-
-mysqli_close($mysqli);
-
+<?
+
+session_start();
+require_once("../../twitteroauth-master/twitteroauth/twitteroauth.php"); /* Path to twitteroauth library */
+include_once '../../inc/connection.php';   /* DB Connection */
+ 
+$twitteruser = "bpm_playlist";
+$notweets = 3;
+ 
+function getConnectionWithAccessToken($cons_key, $cons_secret, $oauth_token, $oauth_token_secret) {
+  $connection = new TwitterOAuth($cons_key, $cons_secret, $oauth_token, $oauth_token_secret);
+  return $connection;
+}
+ 
+$connection = getConnectionWithAccessToken($consumerkey, $consumersecret, $accesstoken, $accesstokensecret);
+ 
+$json = $connection->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=".$twitteruser."&count=".$notweets);
+$Current_Song = strstr($json[0]->text, "playing",true); /* Gets Song Title before "playing ..." */
+$Current_Song = str_replace("#bpmBreaker","",$Current_Song); /* Remove Hashtags */
+		
+		if(preg_match('[@|New Beats Now!|On Now!|Like It?|#TiestoSXM|Ch52|Ch55]', $Current_Song) == true) { /* Cleans random tweets from song list. */
+			echo "Commercial";
+		} else {
+			echo $Current_Song;
+		}
+
+mysqli_close($mysqli);
+
 ?>
