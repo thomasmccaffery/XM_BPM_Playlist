083e6263e4aa5bb0ded3cd3a55b6b83ca312a7c5
thomasmccaffery <tom@thomasmccaffery.com>
2014-08-26 13:08:18 -0400

    UI Change
    Added Full Screen UI, with floating Links and Titles.

--- Live.php
+++ Live.php
@@ -28,21 +28,17 @@ X- link to other main page list
   <head>
     <title>XM_BPM_Playlist Live</title>
 	<link rel="stylesheet" type="text/css" href="assets/styles.css" />
-	<style>body {background: rgb(135, 172, 235); }</style>
+	<style>body{background:black;}</style>
   </head>
   <body>
 	<center>
-		<? include_once 'inc/Header.php';  ?>
+		<div id="Floating_Header"><? include_once 'inc/Header.php';  ?></div>
 		
 		<script src="http://www.youtube.com/iframe_api"></script>
 		<a style="display: none;" class="live" id="HuGbuEv_3AU" href="#"></a>    
 		<div id="container"></div>
 		<div id="Playing_Song">Loading...</div>
 	</center>
-	
-	<div id="Previously_Played">
-		Old Here
-	</div>
 	
 	<script type="text/javascript" src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
 	<script type="text/javascript" src="./assets/xm.js"></script>
