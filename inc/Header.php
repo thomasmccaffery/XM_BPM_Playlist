96d66f6d5bc1f259de6b3a8c102c8bea91b092aa
thomasmccaffery <tom@thomasmccaffery.com>
2014-08-27 16:14:43 -0400

    Fixed Thursday Error
    Changed week from 3 to 4, to represent Thursday (BPM Twitter Take over
    day -- usually non-updated), the previous server I used was on a
    different time zone.

--- inc/Header.php
+++ inc/Header.php
@@ -1,8 +1,8 @@
-<div style="margin-bottom:1%;"><b><u>Songs By: <a href="./">Time</a> | <a href="./?Frequency">Frequency</a> Played</u></b> -- <a href="./Live.php"><b><u>Live</u></b></a> </div>
-	
-	<? 
-	$dw = date( "w"); /* Current Day of the week (need to get Thursdays to show the feed is down - due to '#ThrowBackThursdays/Utopia' takeover.)*/
-	if($dw == 3) { ?>
-		<div id="TBT"><b><u>Note:</u> BPM_XM Feed down on Thursdays due to #ThrowBackThursdays/UtopiaXM channel takeover every Thursday!</b>
-		<br/> Channel will currently just keep repeating the same last song on Thursday.</div>
+<div style="margin-bottom:1%;"><b><u>Songs By: <a href="./">Time</a> | <a href="./?Frequency">Frequency</a> Played</u></b> -- <a href="./Live.php"><b><u>Live</u></b></a> | <a href="./Radio.php"><b><u>Radio</u></b></a> </div>
+	
+	<? 
+	$dw = date( "w"); /* Current Day of the week (need to get Thursdays to show the feed is down - due to '#ThrowBackThursdays/Utopia' takeover.)*/
+	if($dw == 4) { ?>
+		<div id="TBT"><b><u>Note:</u> BPM_XM Feed down on Thursdays due to #ThrowBackThursdays/UtopiaXM channel takeover every Thursday!</b>
+		<br/> Channel will currently just keep repeating the same last song on Thursday.</div>
 	<? } ?>
